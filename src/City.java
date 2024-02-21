a6854ab5cb5c3b260f94de1c13d7a56ca1bd8125
chaimaaenaa <mekayssichaimaaenaa@gmail.com>
2024-02-21 17:19:57 +0100

    les methode

---
+++ src/City.java
@@ -0,0 +1,21 @@
+package src;
+
+public class City {
+    int cityId;
+    String cityName;
+    double currentTemperature;
+    double currentHumidity;
+    double currentWindSpeed;
+    public  void Create(){
+
+    }
+    public  void Read(){
+
+    }
+    public void Update(){
+
+    }
+    public void Delete(){
+
+    }
+}
---
+++ src/CityHistory.java
@@ -0,0 +1,24 @@
+package src;
+
+import java.sql.Date;
+
+public class CityHistory {
+    int historicalDataId;
+    int cityId;
+    Date eventDate;
+    String  temperature ;
+
+    public void Create(){
+
+    }
+    public void Read(){
+
+    }
+    public void Update(){
+
+    }
+    public void Delete(){
+
+    }
+
+}
